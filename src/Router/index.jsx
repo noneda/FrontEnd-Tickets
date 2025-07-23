1fd161dd7a33ebf892ed00d29799a97dec1c7700
noneda <nonejda@gmail.com>
2025-07-23 13:50:20 -0500

    refactor: Fix vercel Production Error

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "@/Layout";
 import ViewAuthContext from "@/Context";
-import ViewDashBoard from "@/Context/DashBoard";
+import ViewDashBoard from "@/Context/Dashboard";
 import SystemApp from "@/Pages/System";
 // import Form from "@/Pages/System/Form"; //TODO: This is a Example how are the Styles from ALL
 import DashBoard from "@/Pages/Dashboard";
